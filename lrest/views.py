276d80f3256ff7e79cf6bd9253c0ebc222ff4c09
Namig <namig600@gmail.com>
2017-04-03 00:21:01 +0400

    fisrt commit

---
+++ lrest/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Stock
+from .serializers import StockSerializer
+from rest_framework.views import APIView
+
+
+class StockList(APIView):
+    def get(self, request):
+        stocks = Stock.objects.all()
+        serializer = StockSerializer(stocks, many=True)
+        return Response(serializer.data)
+
